9344277cdfcdc423293a40890ee251d26c9a02a2
tidux49 <paulbenion49@gmail.com>
2023-10-06 15:43:32 +0200

    fonctionnel

--- FrontEnd/index.js
+++ FrontEnd/index.js
@@ -1,25 +1,39 @@
 
-let gallery = document.querySelector("gallery");
-let figure="";
 
-get();
-async function get (){
-await fetch("http://localhost:5678/api/works").then((response)=>
-    response.json().then((data)=>{
-        console.log(data);
-        for (let projet of data) {
-            let src  = projet.imageUrl;
-            console.log(src);
-            let alt = projet.title;
-            console.log(alt);
-            figure +=`
-            <figure>
-                <img src=${src} alt="photo"${alt}>
-                <figcaption>${alt}</figcaption>
-            </figure>
-            `;
-        };
+let gallery = document.querySelector("#gallery");
+let figure = "";
+let fecthedData
+
+async function fetchget() {
+    await fetch("http://localhost:5678/api/works").then((response) =>
+        response.json().then((data) => {
+            console.log(data);
+            fecthedData = data;
+            tab_maker(data)
+        }));
 }
-    ));}
-    gallery.appendChild(figure); 
 
+function tab_maker(fecthedData) {
+    for (let projet of fecthedData) {
+        let src = projet.imageUrl;
+        console.log(src);
+        let alt = projet.title;
+        console.log(alt);
+        elem = document.createElement("figure");
+        img = document.createElement("img");
+        img.src = src;
+        img.alt = alt;
+        elem.appendChild(img);
+        figcaption = document.createElement("figcaption");
+        figcaption.innerText = alt;
+        elem.appendChild(figcaption);
+
+        gallery.appendChild(elem);
+
+
+    }
+    // console.log(figure)
+
+}
+
+fetchget()
